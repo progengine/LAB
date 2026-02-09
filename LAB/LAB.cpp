020e88cbafbdf1157a3029988e1a879847d9a8de
Ilia Pavlov <sanekteam@yandex.ru>
2026-02-09 23:57:57 +0700

    задание №2 числа Фибоначчи

--- LAB/LAB.cpp
+++ LAB/LAB.cpp
@@ -1,1 +1,34 @@
-﻿
+﻿#include <iostream>;
+using namespace std;
+
+int main() {
+
+	setlocale(LC_ALL, "Russian");
+
+	int f1 = 0;
+	int f2 = 1;
+	int s;
+	int n;
+	
+	cout << "Введите n чисел Фибоначчи" << endl;
+	cin >> n;
+
+	for (int k = 2; k <= n; k++) {
+		int f3 = f1 + f2;
+		s = f3;
+		f1 = f2;
+		f2 = f3;
+	}
+
+	cout << "Введите максимальное число" << endl;
+	int Max;
+	cin >> Max;
+
+	if (s > Max) {
+		cout << "NO!";
+	}
+	else {
+		cout << s;
+	}
+
+}
